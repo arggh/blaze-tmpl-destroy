c4857e751cae1c5eb91f2e4f7cabbe294dc1bced
arggh <alterilari@gmail.com>
2016-06-28 22:53:02 +0300

    Use ViewModel on all templates.

--- client/main.js
+++ client/main.js
@@ -1,23 +1,8 @@
-Template.mainLayout.onCreated(function listOnCreated() {
-  if (!Rows.findOne()) {
-    for (let i = 0; i < 450; i++) {
-      const row = {
-        name: Random.id(40),
-        name1: Random.id(40),
-        name2: Random.id(40),
-        name3: Random.id(40),
-        name4: Random.id(40),
-        name5: Random.id(40),
-        name6: Random.id(40),
-        name7: Random.id(40),
-        name8: Random.id(40),
-        name9: Random.id(40),
-      };
-      const rowId = Rows.insert(row);
-      for (let i = 0; i < 4; i++) {
-        const col = {
-          src: 'http://semantic-ui.com/images/avatar/small/veronika.jpg',
-          rowId: rowId,
+Template.mainLayout.viewmodel({
+  onCreated() {
+    if (!Rows.findOne()) {
+      for (let i = 0; i < 450; i++) {
+        const row = {
           name: Random.id(40),
           name1: Random.id(40),
           name2: Random.id(40),
@@ -27,46 +12,54 @@ Template.mainLayout.onCreated(function listOnCreated() {
           name6: Random.id(40),
           name7: Random.id(40),
           name8: Random.id(40),
-          name9: Random.id(40)
+          name9: Random.id(40),
         };
-        Columns.insert(col);
+        const rowId = Rows.insert(row);
+        for (let i = 0; i < 4; i++) {
+          const col = {
+            src: 'http://semantic-ui.com/images/avatar/small/veronika.jpg',
+            rowId: rowId,
+            name: Random.id(40),
+            name1: Random.id(40),
+            name2: Random.id(40),
+            name3: Random.id(40),
+            name4: Random.id(40),
+            name5: Random.id(40),
+            name6: Random.id(40),
+            name7: Random.id(40),
+            name8: Random.id(40),
+            name9: Random.id(40)
+          };
+          Columns.insert(col);
+        }
       }
     }
   }
 });
 
-Template.list.helpers({
+Template.list.viewmodel({
   rows() {
     return Rows.find();
   },
 });
 
-Template.row.helpers({
+Template.row.viewmodel({
   columns() {
     return Columns.find({
-      rowId: this.r._id
+      rowId: this.r()._id
     });
   }
 });
 
-Template.column.helpers({
+Template.column.viewmodel({
   name() {
     const key = Random.choice(['name','name1','name2','name3','name4','name5','name6','name7','name8','name9'])
-    return this.c[key];
+    return this.c()[key];
   }
 });
 
-Template.nav.helpers({
+Template.nav.viewmodel({
   showList() {
     return Session.get('showList');
-  }
-})
-
-Template.nav.events({
-  'click button#showList'(event, instance) {
-    Session.set('showList', true);
-  },
-  'click button#hideList'(event, instance) {
-    Session.set('showList', false);
   }
 });
